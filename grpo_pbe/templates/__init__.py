30b028c3890fb10f3945fda86fbcbe3b8e7556ab
Arjun-Avadhanam <arjunvijayavadhanam@gmail.com>
2026-05-09 12:52:36 +0530

    refactor: remove try/except ImportError wrapper, all template modules exist

--- grpo_pbe/templates/__init__.py
+++ grpo_pbe/templates/__init__.py
@@ -1,21 +1,18 @@
 """Transformation templates for synthetic data generation."""
-try:
-    from grpo_pbe.templates.string_ops import STRING_TEMPLATES
-    from grpo_pbe.templates.regex_ops import REGEX_TEMPLATES
-    from grpo_pbe.templates.date_ops import DATE_TEMPLATES
-    from grpo_pbe.templates.numeric_ops import NUMERIC_TEMPLATES
-    from grpo_pbe.templates.list_ops import LIST_TEMPLATES
-    from grpo_pbe.templates.dict_ops import DICT_TEMPLATES
-    from grpo_pbe.templates.chained_ops import CHAINED_TEMPLATES
+from grpo_pbe.templates.string_ops import STRING_TEMPLATES
+from grpo_pbe.templates.regex_ops import REGEX_TEMPLATES
+from grpo_pbe.templates.date_ops import DATE_TEMPLATES
+from grpo_pbe.templates.numeric_ops import NUMERIC_TEMPLATES
+from grpo_pbe.templates.list_ops import LIST_TEMPLATES
+from grpo_pbe.templates.dict_ops import DICT_TEMPLATES
+from grpo_pbe.templates.chained_ops import CHAINED_TEMPLATES
 
-    ALL_TEMPLATES = (
-        STRING_TEMPLATES
-        + REGEX_TEMPLATES
-        + DATE_TEMPLATES
-        + NUMERIC_TEMPLATES
-        + LIST_TEMPLATES
-        + DICT_TEMPLATES
-        + CHAINED_TEMPLATES
-    )
-except ImportError:
-    ALL_TEMPLATES = []
+ALL_TEMPLATES = (
+    STRING_TEMPLATES
+    + REGEX_TEMPLATES
+    + DATE_TEMPLATES
+    + NUMERIC_TEMPLATES
+    + LIST_TEMPLATES
+    + DICT_TEMPLATES
+    + CHAINED_TEMPLATES
+)
